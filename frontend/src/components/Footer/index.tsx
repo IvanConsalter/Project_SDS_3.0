2797cfade3bcdd00fc18a49f428f49b5d4447045
IvanConsalter <ivanconsalter1@gmail.com>
2021-05-07 19:03:53 -0300

    home and navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,13 +2,13 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por 
+                <p className="text-light">App developed by 
                     <a href="https://github.com/ivanconsalter" target="_blank" rel="noreferrer"> Ivan Consalter</a>
                 </p>
                 <p className="text-light">
                     <small>
-                        <strong>Semana Spring React</strong><br/>
-                        Evento promovido pela escola DevSuperior: 
+                        <strong>Spring React Week</strong><br/>
+                        Event promoted by the DevSuperior school: 
                         <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a>
                     </small>
                 </p>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,29 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <>
+        <NavBar />
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-4">DS-Sales</h1>
+                <p className="lead">
+                    Analyze your sales performance by different prospects
+                </p>
+                <hr />
+                <p>
+                    This application consists of displaying a dashboard from
+                    data provided by a back end built with Spring Boot.
+                </p>
+                <Link className="btn btn-primary btn-lg" to="/dashboard">
+                    Access DashBoard
+                </Link>
+            </div>
         </div>
+        <Footer />
+        </>
     );
 };
 
